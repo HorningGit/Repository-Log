7e9a3c567b0e81664c1a504eb3cb0e58cb9f2b82
AlexanderHorning <42924284+HorningGit@users.noreply.github.com>
2021-05-22 05:40:43 -0500

    Creation of timeshift script

---
+++ scripts/timeshift.sh
@@ -0,0 +1,10 @@
+# Timeshift Installation Commands
+
+# Install repository keys
+sudo add-apt-repository -y ppa:teejee2008/timeshift
+
+# Update system repositories
+sudo apt-get update
+
+# Install Timeshift Application
+sudo apt-get install timeshift
