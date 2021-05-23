ed1c1fcc3568bc741333a19ef1f0713709cb9a93
AlexanderHorning <42924284+HorningGit@users.noreply.github.com>
2021-05-23 02:20:38 -0500

    Formatting

--- scripts/bravescript.sh
+++ scripts/bravescript.sh
@@ -7,11 +7,15 @@
 
 # Verify/Install apt-transport-https and curl
   sudo apt install apt-transport-https curl
+  
 # Obtain encrypted repository keys
   sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://>
+  
 # Add the repository key
   echo "deb [signed-key=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch>
+  
 # Update our Repositories
     sudo apt update
+    
 # Installe Brave Browser
   sudo apt install brave-browser
